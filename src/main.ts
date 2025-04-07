808a693542890d315dfd509f71fb7a826551884d
zakaria-ba02 <baroudamohmmed@gmail.com>
2025-04-07 16:59:04 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function server() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
     new ValidationPipe({
-      // whitelist: true,
       transform: true
     })
   )
